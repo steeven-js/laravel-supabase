7bea69e50a0f32d0dfa53300b15d60bb81ff3fcc
steeven-js <jacques.steeven@gmail.com>
2025-06-12 17:17:03 -0400

    Simplification template email client - contrôle total utilisateur

--- resources/views/emails/client/custom.blade.php
+++ resources/views/emails/client/custom.blade.php
@@ -1,38 +1,5 @@
 <x-mail::message>
 # {{ $objet }}
 
-Bonjour {{ $client->prenom }} {{ $client->nom }},
-
 {!! nl2br(e($contenu)) !!}
-
----
-
-## 📧 Informations de contact
-
-<x-mail::table>
-| Contact | Information |
-|:--------|:------------|
-| **Expéditeur** | {{ $user->name }} |
-@if($madinia && $madinia->name)
-| **Entreprise** | {{ $madinia->name }} |
-@endif
-@if($madinia && $madinia->telephone)
-| **Téléphone** | {{ $madinia->telephone }} |
-@endif
-@if($madinia && $madinia->email)
-| **Email** | {{ $madinia->email }} |
-@endif
-</x-mail::table>
-
-N'hésitez pas à nous contacter pour toute question ou information complémentaire.
-
-<x-mail::button :url="route('clients.show', $client->id)">
-Voir votre profil client
-</x-mail::button>
-
-Cordialement,<br>
-{{ $user->name }}<br>
-@if($madinia && $madinia->name)
-{{ $madinia->name }}
-@endif
 </x-mail::message>
